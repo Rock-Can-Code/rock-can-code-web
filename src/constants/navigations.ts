d2538ca5d37f7ca0b671634c2bcf90c6a2fb984e
adriel arocha <adrielarochaoronoz@gmail.com>
2024-02-17 18:17:34 +0000

    base routes created

---
+++ src/constants/navigations.ts
@@ -0,0 +1,17 @@
+
+
+export const NAVIGATION_LIST = [
+    { name: "Projects", href: "/projects" },
+    { name: "Contact", href: "/contact" },
+    { name: "About us", href: "/about" },
+    { name: "Services", href: "/services" },
+    { name: "blog", href: "/blog" },
+];
+
+export enum NAVIGATION_ENUM {
+Projects='/projects',
+Contact='/contact',
+AboutUs='/aboutus',
+Services='/services',
+blog='/blog',
+}
---
+++ src/views/services/view.tsx
@@ -0,0 +1,9 @@
+import { BaseContainer } from "../common/components/baseContainer"
+
+export const ServicesView = ()=> {
+    return (<>
+        <BaseContainer>
+            <h2 className="text-white">Services</h2>
+        </BaseContainer>
+    </>)
+}
