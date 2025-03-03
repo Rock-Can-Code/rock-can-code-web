ea3db98d79d0ba7fedb7128de3a5c8e3e4f0ae94
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2025-03-03 17:25:27 +0100

    :sparkles: fill projects page with content

--- app/(web)/projects/page.tsx
+++ app/(web)/projects/page.tsx
@@ -24,12 +24,9 @@ export default async function ProjectsPage() {
           <Card>
             <Link href={`/projects/`}>
               <article className="relative w-full h-full p-4 md:p-8">
-                <div className="flex items-center justify-between gap-2">
-                  <div className="text-xs text-zinc-100">
-                    <span>SOON</span>
-                  </div>
-                  <span className="flex items-center gap-1 text-xs text-zinc-500">
-                    <span>SOON</span>
+                <div className="flex justify-end">
+                  <span className="text-xs text-zinc-500">
+                    <span>2025</span>
                   </span>
                 </div>
 
@@ -37,15 +34,13 @@ export default async function ProjectsPage() {
                   id="featured-post"
                   className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
                 >
-
-                  titulo
+                  Freedom AI.
                 </h2>
                 <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
-
-                  descripcion
+                  Uncensored and local runned AI.
                 </p>
-                <div className="absolute bottom-4 md:bottom-8">
-                  <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                <div className="mt-4">
+                  <p className="text-zinc-200 hover:text-zinc-50">
                     Read more <span aria-hidden="true">&rarr;</span>
                   </p>
                 </div>
@@ -54,26 +49,38 @@ export default async function ProjectsPage() {
           </Card>
 
           <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+            
+            <Card>
+              <Link href={`/projects/`}>
+                <article className="relative w-full h-full p-4 md:p-8">
+                  <div className="flex justify-end">
+                    <span className="text-xs text-zinc-500">
+                      <span>SOON</span>
+                    </span>
+                  </div>
 
-            <span>SOON</span>
+                  <h2
+                    id="featured-post"
+                    className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+                  >
+                    LUM
+                  </h2>
+                  <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                    The future of HRMs.
+                  </p>
+                  <div className="mt-4">
+                    <p className="text-zinc-200 hover:text-zinc-50">
+                      Read more <span aria-hidden="true">&rarr;</span>
+                    </p>
+                  </div>
+                </article>
+              </Link>
+            </Card>
           </div>
         </div>
+        
         <div className="hidden w-full h-px md:block bg-zinc-800" />
 
-        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-          <div className="grid grid-cols-1 gap-4">
- 
-              <span>SOON</span>
-          </div>
-          <div className="grid grid-cols-1 gap-4">
- 
-              <span>SOON</span>
-          </div>
-          <div className="grid grid-cols-1 gap-4">
-
-              <span>SOON</span>
-          </div>
-        </div>
       </div>
     </div>
   );
