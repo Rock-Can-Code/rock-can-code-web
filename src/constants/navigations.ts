462a2321db2a4bc24c18844c1eb411bee9959644
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2025-03-19 22:51:35 +0100

    ✨ Removed Services section from navbar

--- src/constants/navigations.ts
+++ src/constants/navigations.ts
@@ -5,8 +5,6 @@ export const NAVIGATION_LIST = [
     { name: "Projects", href: "/projects" },
     { name: "Contact", href: "/contact" },
     { name: "About us", href: "/about" },
-    { name: "Services", href: "/services" },
-    { name: "blog", href: "/blog" },
 ];
 
 export enum NAVIGATION_ENUM {
@@ -14,6 +12,4 @@ Home='/',
 Projects='/projects',
 Contact='/contact',
 AboutUs='/about',
-Services='/services',
-blog='/blog',
 }
